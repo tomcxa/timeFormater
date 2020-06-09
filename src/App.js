f3a9d85c88a34971b491e24ee0634969c4b717e5
tomcxa <tomcxa@yandex.ru>
2020-06-09 15:16:05 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
 }
 
-export default App;
+function DateTimePretty(Component, dateFormater) {
+    return (props) => {
+        const date = dateFormater(props.date)
+        return <Component {...props} date={date} />
+    }
+}
+
+function dateFormater(date) {
+    const now = moment(Date.now())
+    const parsedDate = moment(date)
+    const diff = now.diff(parsedDate, 'hours')
+    const day = (diff / 24).toFixed(0)
+    if (diff < 1) {
+        const min = (diff * 60).toFixed(0)
+        return `${min} минут назад`
+    }
+    if (diff > 1 && diff < 24) {
+        const hour = diff.toFixed(0)
+        return `${hour} часов назад`
+    }
+    return `${day} дней назад`
+}
+
+const WithFormatDateTime = DateTimePretty(DateTime, dateFormater)
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            {/* <DateTime date={props.date} /> */}
+            <WithFormatDateTime date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video key={item.url} url={item.url} date={item.date} />);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2020-06-09 15:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2020-06-09 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
